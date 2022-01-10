aed7ef1ee7571396dda572cd6093aa1d10931c99
Francisco Suarez <56086678+suarezfco65@users.noreply.github.com>
2022-01-10 13:25:53 -0400

    Update audible.js

--- audible.js
+++ audible.js
@@ -10,7 +10,7 @@ class audible extends HTMLElement {
     super();
     const html = `
     <style>
-      .btn-success {
+      .btn-verde {
         font-weight: 400;
         line-height: 1.5;
         text-align: center;
@@ -22,7 +22,7 @@ class audible extends HTMLElement {
         color:white; background-color: green; border-color:green;
         cursor:pointer;
       }
-      .btn-danger {
+      .btn-rojo {
         font-weight: 400;
         line-height: 1.5;
         text-align: center;
@@ -35,22 +35,22 @@ class audible extends HTMLElement {
         cursor:pointer;
       }
       .bottomRight{
-        position: fixed;
+        position:absolute;
         bottom:5px;
         right:5px;
       }
       .bottomLeft{
-        position: fixed;
+        position:absolute;
         bottom:5px;
         left:5px;
       }
       .topRight{
-        position: fixed;
+        position:absolute;
         top:20px;
         right:5px;
       }
       .topLeft{
-        position: fixed;
+        position:absolute;
         top:20px;
         left:5px;
       }
@@ -80,7 +80,7 @@ class audible extends HTMLElement {
         </tr>
         <tr>
             <td>
-                <button class="btn-success" type="button" id="audible-id-comenzar" title="Reiniciar" onclick="this.click();">
+                <button class="btn-verde" type="button" id="audible-id-comenzar" title="Reiniciar" onclick="this.click();">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-left" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
                         <path fill-rule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
@@ -88,14 +88,14 @@ class audible extends HTMLElement {
                 </button>
             </td>
             <td>
-                <button class="btn-success" type="button" id="audible-id-playPause" title="play" onclick="this.playAudio();">
+                <button class="btn-verde" type="button" id="audible-id-playPause" title="play" onclick="this.playAudio();">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16">
                         <path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z"/>
                     </svg>
                 </button>
             </td>
             <td>
-                <button class="btn-success" type="button" id="audible-id-terminar" title="Finalizar" onclick="this.click();">
+                <button class="btn-verde" type="button" id="audible-id-terminar" title="Finalizar" onclick="this.click();">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-right" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z"/>
                         <path fill-rule="evenodd" d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708z"/>
@@ -103,7 +103,7 @@ class audible extends HTMLElement {
                 </button>
             </td>
             <td>
-                <button class="btn-success" type="button" id="audible-id-subirVol" title="Volumen +" onclick="this.click();">
+                <button class="btn-verde" type="button" id="audible-id-subirVol" title="Volumen +" onclick="this.click();">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-volume-up" viewBox="0 0 16 16">
                         <path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/>
                         <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/>
@@ -112,14 +112,14 @@ class audible extends HTMLElement {
                 </button>
             </td>
             <td>
-                <button class="btn-success" type="button" id="audible-id-bajarVol" title="Volumen -" onclick="this.click();">
+                <button class="btn-verde" type="button" id="audible-id-bajarVol" title="Volumen -" onclick="this.click();">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-volume-down" viewBox="0 0 16 16">
                         <path d="M9 4a.5.5 0 0 0-.812-.39L5.825 5.5H3.5A.5.5 0 0 0 3 6v4a.5.5 0 0 0 .5.5h2.325l2.363 1.89A.5.5 0 0 0 9 12V4zM6.312 6.39 8 5.04v5.92L6.312 9.61A.5.5 0 0 0 6 9.5H4v-3h2a.5.5 0 0 0 .312-.11zM12.025 8a4.486 4.486 0 0 1-1.318 3.182L10 10.475A3.489 3.489 0 0 0 11.025 8 3.49 3.49 0 0 0 10 5.525l.707-.707A4.486 4.486 0 0 1 12.025 8z"/>
                     </svg>                        
                 </button>
             </td>
             <td>
-                <button class="btn-success" type="button" id="audible-id-callar" title="Silenciar" onclick="this.click();">
+                <button class="btn-verde" type="button" id="audible-id-callar" title="Silenciar" onclick="this.click();">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-volume-mute" viewBox="0 0 16 16">
                         <path d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06zM6 5.04 4.312 6.39A.5.5 0 0 1 4 6.5H2v3h2a.5.5 0 0 1 .312.11L6 10.96V5.04zm7.854.606a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0z"/>
                     </svg>
@@ -151,7 +151,7 @@ class audible extends HTMLElement {
     oAudio.terminar.click = function () {
       oAudio.currentTime = oAudio.duration;
       oAudio.avance.value = oAudio.duration;
-      oAudio.playPause.className = 'btn-success';
+      oAudio.playPause.className = 'btn-verde';
       oAudio.playPause.onclick = oAudio.playPause.playAudio;
       oAudio.playPause.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16">
       <path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z"/>
@@ -160,7 +160,7 @@ class audible extends HTMLElement {
 
     oAudio.playPause = document.getElementById('audible-id-playPause');
     oAudio.playPause.playAudio = function () {
-      oAudio.playPause.className = 'btn-danger';
+      oAudio.playPause.className = 'btn-rojo';
       oAudio.playPause.title = 'Pausa';
       oAudio.playPause.onclick = oAudio.playPause.pauseAudio;
       oAudio.playPause.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pause" viewBox="0 0 16 16">
@@ -170,7 +170,7 @@ class audible extends HTMLElement {
     };
 
     oAudio.playPause.pauseAudio = function () {
-      oAudio.playPause.className = 'btn btn-success';
+      oAudio.playPause.className = 'btn btn-verde';
       oAudio.playPause.title = 'Play';
       oAudio.playPause.onclick = oAudio.playPause.playAudio;
       oAudio.playPause.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16">
